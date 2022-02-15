27b8b4557bb0ea6306ecaa1a6ec8cf5313f2c6cd
tseitz <tdseitz10@gmail.com>
2022-02-14 21:35:31 -0600

    Turn off side secret

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
-	import side from './side-test-removebg.png';
+	// import side from './side-test-removebg.png';
 </script>
 
 <Header />
 
 <main class="flex flex-col items-center w-full bg-missy-500">
 	<slot />
-	<div class="side-secret fixed flex justify-between">
+	<!-- <div class="side-secret fixed flex justify-between">
 		<img src={side} alt="Missy Midwest Hi Left" class="relative opacity-70 h-screen" />
 		<img src={side} alt="Missy Midwest Hi Right" class="relative opacity-70 h-screen" />
-	</div>
+	</div> -->
 </main>
 
 <style>
-	.side-secret {
+	/* .side-secret {
 		width: 100vw;
 		min-width: 3300px;
 		z-index: -1;
@@ -27,5 +27,5 @@
 	.side-secret img:nth-child(2) {
 		right: -220px;
 		transform: scaleX(-1) rotate(36deg);
-	}
+	} */
 </style>

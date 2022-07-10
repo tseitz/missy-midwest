bd189a218f28d33a06023e2d5b1647a96f6953a9
tseitz <tdseitz10@gmail.com>
2022-07-10 10:29:26 -0500

    Remove missy header from form

--- src/lib/contact/Contact.svelte
+++ src/lib/contact/Contact.svelte
@@ -83,7 +83,7 @@
 	</div>
 	<div class="bg-slate-100 rounded-md p-8">
 		<form on:submit={handleSubmit}>
-			<label class="text-xl missy-header text-missy-500 mb-2" for="name">Name</label>
+			<label class="text-xl text-missy-500 mb-2" for="name">Name</label>
 			<input
 				id="name"
 				name="name"
@@ -95,7 +95,7 @@
 				<small class="text-red-500">{$errors.name}</small>
 			{/if}
 
-			<label class="text-xl missy-header text-missy-500 mt-4 mb-2" for="email">Email</label>
+			<label class="text-xl text-missy-500 mt-4 mb-2" for="email">Email</label>
 			<input
 				id="email"
 				name="email"
@@ -107,9 +107,7 @@
 				<small class="text-red-500">{$errors.email}</small>
 			{/if}
 
-			<label class="text-xl missy-header text-missy-500 mt-4 mb-2" for="phone"
-				>Phone (Optional)</label
-			>
+			<label class="text-xl text-missy-500 mt-4 mb-2" for="phone">Phone (Optional)</label>
 			<input
 				id="phone"
 				name="phone"
@@ -121,7 +119,7 @@
 				<small class="text-red-500">{$errors.phone}</small>
 			{/if}
 
-			<label class="text-xl missy-header text-missy-500 mt-4 mb-2" for="message">Message</label>
+			<label class="text-xl text-missy-500 mt-4 mb-2" for="message">Message</label>
 			<textarea
 				id="message"
 				name="message"

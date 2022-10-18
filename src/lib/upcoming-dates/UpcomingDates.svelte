d4dfbefb79b9e84f706f05a477050d439338851b
tseitz <tdseitz10@gmail.com>
2022-10-18 11:59:12 -0500

    Update dates and missy mix

--- src/lib/upcoming-dates/UpcomingDates.svelte
+++ src/lib/upcoming-dates/UpcomingDates.svelte
@@ -1,33 +1,68 @@
 <script lang="ts">
-	import frankyAndLouies from './images/franky-and-louies.webp';
-	// import islandParty from './images/island-party.webp';
-	import unitedGroove from './images/united-groove.webp';
+	import faucette from './images/faucette.webp';
+	import subSessions from './images/subsessions.webp';
+	import backwoods from './images/backwoods.webp';
+	import halloween from './images/halloween.webp';
+	import anniversary from '../press-kit/images/missy-guitar-pic.png';
 </script>
 
 <section id="dates" class="max-w-screen-2xl w-full pt-20 pb-16">
 	<h2 class="text-slate-100 text-4xl mb-8 md:mb-12 italic">Upcoming Dates!</h2>
 	<div class="grid xl:grid-cols-3 lg:grid-cols-2 gap-10 text-slate-100">
-		<!-- <div
+		<div
 			class="event h-96 w-full bg-slate-100 rounded-md flex flex-col justify-end"
-			style="background: url({frankyAndLouies}) rgb(241 245 249) no-repeat center 36%;opacity:0.98;background-size: 100%;"
+			style="background: url({faucette}) rgb(241 245 249) no-repeat center 36%;opacity:0.98;background-size: 100%;"
 		>
 			<div class="description bg-slate-100 px-8 py-4 rounded-b-md text-missy-500">
-				<p class="text-2xl missy-header">Franky & Louie's Beachfront Bar</p>
-				<p class="text-md">Saturday, August 20th 1pm-5pm</p>
-				<p class="text-sm">Franky & Louie's 1028 Deer Valley Rd, Sunrise Beach, MO 65079</p>
+				<p class="text-2xl missy-header">Faucette Festival</p>
+				<p class="text-md">Saturday, October 22nd 12-8pm</p>
+				<p class="text-sm">Faucette Park</p>
+				<p class="text-sm">100 Riverfront Pl, North Little Rock, AR 72114</p>
 			</div>
 		</div>
 		<div
 			class="event h-96 w-full bg-slate-100 rounded-md flex flex-col justify-end"
-			style="background-image: url({unitedGroove});background-repeat: no-repeat;background-size:cover;opacity:0.98;background-position:center;"
+			style="background: url({halloween}) rgb(241 245 249) no-repeat center 36%;opacity:0.98;background-size: 100%;"
 		>
 			<div class="description bg-slate-100 px-8 py-4 rounded-b-md text-missy-500">
-				<p class="text-2xl missy-header">United Groove Presents</p>
-				<p class="text-md">August 27th</p>
-				<p class="text-sm">The Labyrinth 619 S Spring St. Little Rock, AR</p>
+				<p class="text-2xl missy-header">Missy Mix: The Spooky Special Vol. II</p>
+				<p class="text-md">Monday, October 24th</p>
+				<p class="text-sm">Soundcloud</p>
 			</div>
-		</div> -->
-		No scheduled events at this time. Use the contact form below to book Missy!
+		</div>
+		<div
+			class="event h-96 w-full bg-slate-100 rounded-md flex flex-col justify-end"
+			style="background: url({backwoods}) rgb(241 245 249) no-repeat center 36%;opacity:0.98;background-size: 100%;"
+		>
+			<div class="description bg-slate-100 px-8 py-4 rounded-b-md text-missy-500">
+				<p class="text-2xl missy-header">Backwoods On The Road Tour</p>
+				<p class="text-md">Friday, October 28th 8pm-12am</p>
+				<p class="text-sm">Stickyz Rock'n'Roll Chicken</p>
+				<p class="text-sm">107 River Market Ave, Little Rock, AR 72201</p>
+			</div>
+		</div>
+		<div
+			class="event h-96 w-full bg-slate-100 rounded-md flex flex-col justify-end"
+			style="background-image: url({anniversary});background-repeat: no-repeat;background-size:cover;opacity:0.98;background-position:center;"
+		>
+			<div class="description bg-slate-100 px-8 py-4 rounded-b-md text-missy-500">
+				<p class="text-2xl missy-header">Missy Mix: 1 Year Anniversary!</p>
+				<p class="text-md">Friday, November 4th</p>
+				<p class="text-sm">Soundcloud</p>
+			</div>
+		</div>
+		<div
+			class="event h-96 w-full bg-slate-100 rounded-md flex flex-col justify-end"
+			style="background-image: url({subSessions});background-repeat: no-repeat;background-size:cover;opacity:0.98;background-position:center;"
+		>
+			<div class="description bg-slate-100 px-8 py-4 rounded-b-md text-missy-500">
+				<p class="text-2xl missy-header">SUB SESSIONS</p>
+				<p class="text-md">Saturday, November 5th 9pm</p>
+				<p class="text-sm">Hopkins Icehouse</p>
+				<p class="text-sm">301 E 3rd St, Texarkana, AR 71854</p>
+			</div>
+		</div>
+		<!-- No scheduled events at this time. Use the contact form below to book Missy! -->
 	</div>
 </section>
 

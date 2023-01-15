dccd23da5074e02508ae222a7f5c77145a04c3aa
tseitz <tdseitz10@gmail.com>
2023-01-15 14:03:08 -0600

    upgrade sveltekit to 1.1.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
-import preprocess from 'svelte-preprocess';
-
-import netlify from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-auto';
+// import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({}),
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: netlify(),
-
-		prerender: {
-			enabled: true
-		}
+		adapter: adapter()
 	}
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,12 @@
-// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-
-import preprocess from 'svelte-preprocess';
-
-import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-
-	preprocess: preprocess({}),
-
 	kit: {
-		adapter: netlify(),
-
-		prerender: {
-			enabled: true
-		},
-
 		optimizeDeps: {
 			include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
 		}
-	},
-
-	build: {
-		target: 'es2019',
-		minify: 'esbuild'
 	}
 };
 
